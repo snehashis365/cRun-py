bcd66ccd1b5a5b5aeb2337b4742e141fefbab7ab
snehashis365 <snehashis.2000@gmail.com>
2020-08-18 00:11:55 +0000

    Update setup.py and help message with code cleanup

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ with open("README.md", "r") as fh:
 
 setup(
     name="crun-py",
-    version="0.0.2",
+    version="0.0.3",
     author="Snehashis Sarkar",
     author_email="snehashis.2000@gmail.com",
     description="C Programming Simplified",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/snehashis365/cRun",
+    project_urls={
+        "GitHub repository": "https://github.com/snehashis365/cRun-py",
+    },
     packages=["src"],
+    install_requires=[
+          'windows-curses',
+      ],
     include_package_data=True,
     entry_points={
         "console_scripts": [
@@ -23,7 +29,11 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Android",
     ],
     python_requires='>=3.6',
 )
--- src/cRun.py
+++ src/cRun.py
@@ -17,7 +17,6 @@ CLEANUP = False
 SHOW_TIME = False
 TEST_MODE = False
 TEST_RETURN = 0
-TEST_KEYS = []
 MAX_FILE_NAME = 0
 # Color codes
 LGREEN = "\033[1;32m"
@@ -25,7 +24,15 @@ RED = "\033[0;31m"
 BLUE = "\033[0;34m"
 LBLUE = "\033[0;36m"
 NORMAL = "\033[0m"
-BANNER_ART = "                 ____                __  __\n          _____ / __ \\ __  __ ____   \\ \\ \\ \\\n         / ___// /_/ // / / // __ \\   \\ \\ \\ \\\n        / /__ / _, _// /_/ // / / /   / / / /\n        \\___//_/ |_| \\__,_//_/ /_/   /_/ /_/\n\n                          - by snehashis365"
+# BANNER_ART = "                 ____                __  __\n          _____ / __ \\ __  __ ____   \\ \\ \\ \\\n         / ___// /_/ // / / // __ \\   \\ \\ \\ \\\n        / /__ / _, _// /_/ // / / /   / / / /\n        \\___//_/ |_| \\__,_//_/ /_/   /_/ /_/\n\n                          - by snehashis365"
+BANNER_ART = r"""
+                 ____                __  __
+          _____ / __ \ __  __ ____   \ \ \ \
+         / ___// /_/ // / / // __ \   \ \ \ \
+        / /__ / _, _// /_/ // / / /   / / / /
+        \___//_/ |_| \__,_//_/ /_/   /_/ /_/
+                                -by snehashis365
+"""
 
 
 def clear():  # Executes command depending on OS
@@ -35,10 +42,19 @@ def clear():  # Executes command depending on OS
         os.system("clear")
 
 
+def get_help():
+    print(f"cRun Version:{VERSION}(test-release) by snehashis365")
+    print("""This script will compile the files specified and generate object files with same name as the C file and
+    Execute them in the order entered.\n""")
+    print(f"Usage: {sys.argv[0]} [-h help] [-c compile] [-r execute] [-m menu ] [-t test] [-d cleanup] *filename.c")
+    print("The above command will consider only the files specified\n")
+    print(f"Usage: {sys.argv[0]} [-c compile] [-t test] [-d cleanup]")
+    print("The above command will consider all .c files in the working directory")
+
+
 def banner():  # Builds banner
     print(LBLUE, end="")
-    print(BANNER_ART)
-    # print(f"{LGREEN}                          - by snehashis365{NORMAL}")
+    print(BANNER_ART, end="")
     print(f"Version : {LGREEN}{VERSION}(test-release){NORMAL}")
     print("Re-Compile : ", end="")
     if COMPILE:
@@ -137,66 +153,75 @@ def build_menu(file_list):
 def test_banner(stdscr):  # This will replace the banner function once finished
     stdscr.clear()
     try:
-        stdscr.attron(curses.color_pair(3))
         y = 0
         for line in BANNER_ART.splitlines():
-            stdscr.addstr(y, 0, line)
+            stdscr.addstr(y, 0, line, curses.color_pair(6))
             y += 1
+        stdscr.addstr(y, 10, f"{VERSION}(test-release)", curses.color_pair(3))
+        if COMPILE:
+            stdscr.addstr(y + 1, 13, "On", curses.color_pair(5))
+        else:
+            stdscr.addstr(y + 1, 13, "Off")
+        if CLEANUP:
+            stdscr.addstr(y + 2, 15, "On", curses.A_BLINK)
+        else:
+            stdscr.addstr(y + 2, 15, "Off")
+        stdscr.addstr(y, 0, "Version : ", curses.color_pair(5))
+        stdscr.addstr(y + 1, 0, "Re-Compile : ")
+        stdscr.addstr(y + 2, 0, "Auto Cleanup : ")
         stdscr.refresh()
-        stdscr.attroff(curses.color_pair(3))
-
-    except curses.error as e:
+    except curses.error:
         pass
 
 
 def test(stdscr, file_list):  # This will replace the build menu function once finished
-    # stdscr = curses.initscr()
-    global TEST_RETURN, TEST_KEYS, MAX_FILE_NAME
+    global TEST_RETURN, MAX_FILE_NAME
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_RED, curses.COLOR_BLACK)
     sel_index = 0
 
     while True:
         h, w = stdscr.getmaxyx()
         test_banner(stdscr)
-        stdscr.addstr(h-1, 0, f"h={h},w={w},sel={sel_index}")
+        if sel_index < len(file_list):
+            stdscr.addstr(h-1, 0, f"Press Enter to select -> {file_list[sel_index]}")
+        else:
+            stdscr.addstr(h - 1, 0, f"Press Enter to confirm Exit")
         index = 0
-        y, x = 10, 0
+        y, x = 12, 0
         for file in file_list:
             pair = 1
             if os.path.exists(CACHE_FOLDER + file[:-2] + ".out"):
-                if index == sel_index:
-                    pair = 4
-                else:
-                    pair = 3
-            elif index == sel_index:
-                pair = 2
+                pair = 3
             stdscr.attron(curses.color_pair(pair))
             try:
                 if y == h-3:
-                    y = 10
+                    y = 12
                     x += MAX_FILE_NAME+3  # Reset X and Y
-                stdscr.addstr(y, x, f"> {file[:-2]}")
-            except curses.error as e:
+                if index == sel_index:
+                    stdscr.addstr(y, x, f"> {file[:-2]}", curses.A_STANDOUT)
+                else:
+                    stdscr.addstr(y, x, f"> {file[:-2]}")
+            except curses.error:
                 pass
             index += 1
             y += 1
             stdscr.attroff(curses.color_pair(pair))
         try:
             if sel_index == len(file_list):
-                stdscr.attron(curses.color_pair(2))
-                stdscr.addstr(h-2, (w//2) - 2, "Exit")
-                stdscr.attroff(curses.color_pair(2))
+                stdscr.addstr(h-2, (w//2) - 2, "Exit", curses.A_REVERSE)
             else:
                 stdscr.addstr(h-2, (w//2) - 2, "Exit")
-        except curses.error as e:
+        except curses.error:
             pass
         # stdscr.refresh()
         key = stdscr.getch()
-        TEST_KEYS.append(key)
         if key == curses.KEY_ENTER or key in [10, 13]:
             break
         elif key == curses.KEY_UP:
@@ -209,17 +234,15 @@ def test(stdscr, file_list):  # This will replace the build menu function once f
                 sel_index += 1
             else:
                 sel_index = 0
-        elif key == curses.KEY_RIGHT and sel_index+(h-10)-3 <= len(file_list):
-            sel_index += (h - 10) - 3
-        elif key == curses.KEY_LEFT and sel_index-((h-10)-3) >= 0:
-            sel_index -= (h - 10) - 3
+        elif key == curses.KEY_RIGHT and sel_index+(h-12)-3 <= len(file_list):
+            sel_index += (h - 12) - 3
+        elif key == curses.KEY_LEFT and sel_index-((h-12)-3) >= 0:
+            sel_index -= (h - 12) - 3
         elif key == 27:
             try:
-                stdscr.attron(curses.color_pair(2))
-                stdscr.addstr(h - 2, (w // 2) - 5, "Exiting...")
-                stdscr.attroff(curses.color_pair(2))
+                stdscr.addstr(h - 2, (w // 2) - 7, "...Exiting...", curses.A_REVERSE)
                 stdscr.refresh()
-            except curses.error as e:
+            except curses.error:
                 pass
             time.sleep(1)
             sys.exit()
@@ -227,7 +250,7 @@ def test(stdscr, file_list):  # This will replace the build menu function once f
 
 
 def main():
-    global EXECUTE, COMPILE, BUILD_MENU, CLEANUP, SINGLE_FILE, TEST_MODE, TEST_RETURN, TEST_KEYS, MAX_FILE_NAME
+    global EXECUTE, COMPILE, BUILD_MENU, CLEANUP, SINGLE_FILE, TEST_MODE, TEST_RETURN, MAX_FILE_NAME
     # Handle options
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hcrmtvdsiu",
@@ -238,7 +261,7 @@ def main():
         sys.exit(2)
     for opt, a in opts:
         if opt in ["-h", "--help"]:
-            print("Help message here")
+            get_help()
             sys.exit()
         elif opt in ["-c", "--compile"]:
             COMPILE = True
@@ -290,7 +313,6 @@ def main():
                     if TEST_RETURN < len(c_files):
                         build_submenu(c_files[TEST_RETURN])
                     else:
-                        print(TEST_KEYS)
                         print("Exiting...")
                         break
             else:
